42e4310356a891edd5d6782f12e89ded8e464768
HKI <rldlsdld24@gmail.com>
2018-09-24 21:58:25 +0900

    추가

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://kini21.github.io/PHP-CodeIgniter-board/';
 
 
 /*
